a07e3eaf1d2d5e9a3a7a929fdcbe3c0047814ef8
devrba <barrios.dev@gmail.com>
2021-04-18 10:25:31 +0200

    april 21 update

--- src/server/seeds/003_00_seed-users-roles.js
+++ src/server/seeds/003_00_seed-users-roles.js
@@ -4,8 +4,10 @@ exports.seed = function(knex) {
     .then(function() {
       return knex('user_roles').insert([
         { fk_user_id: 1, fk_role_id: 1 },
-        { fk_user_id: 2, fk_role_id: 2 },
+        { fk_user_id: 2, fk_role_id: 1 },
         { fk_user_id: 3, fk_role_id: 3 },
+        { fk_user_id: 4, fk_role_id: 3 },
+        { fk_user_id: 5, fk_role_id: 1 },
       ]);
     });
 };
